c039279b4416645b2ef996e1d9c99a1c4a9088af
Dean Phillips <dephillips1977@gmail.com>
2024-08-24 11:18:59 -0500

    Added text examples

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
 import streamlit as st
 
-st.text("Hello Streamlit World!")
+# Title
+st.title("Streamlit Basics")
+
+# Header
+st.header("Main header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+# Markdown
+st.markdown("This is markdown **text**")
+st.markdown("# Header1")
+st.markdown("## Header2")
+st.markdown("### Header3")
+
+# Caption
+st.caption("This is a caption")
+
+# Code block
+st.code("""
+import pandas as pd
+pd.read_csv(my_csv_file)"""
+)
+
+# Preformatted text
+st.text("Some text")
+
+# LaTeX
+st.latex("x = 2^3")
+
+# Divider
+st.text("text above the divider")
+st.divider()
+st.text("text below the divider")
+
+st.write("Some written text")
