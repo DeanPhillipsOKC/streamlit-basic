1bb33b7685f6be9228af61c9727deac96bdacb2c
Dean Phillips <dephillips1977@gmail.com>
2024-08-25 08:33:07 -0500

    Added layout elements

--- main.py
+++ main.py
@@ -182,3 +182,48 @@ Are you bringing your own wine: {"yes" if wine else "no"}
 
 {allergies}
 """)
+        
+st.header("Layout elements")
+
+
+
+with st.sidebar:
+    st.subheader("sidebar")
+    st.write("Text in the sidebar")
+
+st.subheader("columns")
+
+col1, col2, col3 = st.columns(3)
+
+col1.write("Text in a column")
+
+slider2 = col2.slider("Choose a number", min_value=0, max_value=10)
+
+col3.write(slider2)
+
+st.divider()
+st.subheader("Tabs")
+
+tab1, tab2 = st.tabs(["Line plot", "Bar plot"])
+
+with tab1:
+    tab1.write("A line plot")
+    st.line_chart(df, x="year", y=["col1", "col2", "col3"])
+
+with tab2:
+    tab2.write("A bar plot")
+    st.bar_chart(df, x="year", y=["col1", "col2", "col3"])
+
+st.divider()
+st.subheader("Expander")
+
+with st.expander("Click to expand"):
+    st.write("I am text that you only see when you expand")
+
+st.divider()
+st.subheader("Container")
+
+with st.container():
+    st.write("This is inside the container")
+
+st.write("This is outside the container")
